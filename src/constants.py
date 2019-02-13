f308b47f014582851e26516dc70d3943432f4f50
Niels Boecker <nielsboecker@gmail.com>
2019-02-13 20:39:40 +0000

    #1: Improve notebook

--- src/constants.py
+++ src/constants.py
@@ -5,4 +5,5 @@ DATA_TRAINING_PATH = DATA_BASE_PATH + 'train.jsonl'
 DATA_DEV_UNLABELED_PATH = DATA_BASE_PATH + 'shared_task_dev_public.jsonl'
 DATA_DEV_LABELED_PATH = DATA_BASE_PATH + 'shared_task_dev.jsonl'
 
-DATA_COLUMNS = ['id', 'verifiable', 'label', 'claim', 'evidence']
+DATA_COLUMNS_UNLABELED = ['id', 'claim']
+DATA_COLUMNS_LABELED = ['id', 'verifiable', 'label', 'claim', 'evidence']
