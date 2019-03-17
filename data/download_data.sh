7dccb20300fcf96a4dc27c635b766e7115eaf146
Niels Boecker <nielsboecker@gmail.com>
2019-03-17 19:13:48 +0000

    Fix typo

--- data/download_data.sh
+++ data/download_data.sh
@@ -4,7 +4,7 @@
 wget https://s3-eu-west-1.amazonaws.com/fever.public/wiki-pages.zip &&
     unzip wiki-pages.zip &&
     rm -r __MACOSX &&
-    rm licence.html &&
+    rm license.html &&
     rm wiki-pages.zip
 
 # Shared Task Development Dataset (Labelled)
