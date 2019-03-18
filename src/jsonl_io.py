93b884e8767c4d4f8044b271e8196e4022808933
Niels Boecker <nielsboecker@gmail.com>
2019-03-18 15:23:46 +0000

    #2: Create dir if not exists

--- src/jsonl_io.py
+++ src/jsonl_io.py
@@ -1,19 +1,16 @@
 import json
+import os
 
 import jsonlines
 import pandas as pd
 from termcolor import colored
 
 
-#def read_jsonl_and_map_to_dict(input_path: str) -> dict:
-#    # NOTE: This is very specific, only lists of lists of format [id: val}, not fit for every jsonl
-#    items = {}
-#    with jsonlines.open(input_path) as reader:
-#        for line in reader:
-#            key = line[0]
-#            value = line[1]
-#            items[key] = value
-#    return items
+def create_dir_if_not_exists(file_path: str):
+    dir_path = os.path.dirname(file_path)
+    if not os.path.exists(dir_path):
+        print('Creating {} directory'.format(dir_path))
+        os.makedirs(dir_path, exist_ok=True)
 
 
 def read_dict_from_json(input_path: str) -> dict:
@@ -23,6 +20,7 @@ def read_dict_from_json(input_path: str) -> dict:
 
 
 def write_dict_to_json(output_path: str, data: dict):
+    create_dir_if_not_exists(output_path)
     with open(output_path, 'w') as fp:
         json.dump(data, fp)
 
@@ -38,6 +36,7 @@ def read_jsonl_and_map_to_df(input_path: str, columns: list = None) -> pd.DataFr
 
 
 def write_list_to_jsonl(output_path: str, data: list):
+    create_dir_if_not_exists(output_path)
     with jsonlines.open(output_path, mode='w') as writer:
         writer.write_all(data)
     print(colored('Wrote {} lines to "{}"'.format(len(data), output_path), attrs=['bold']))
