6857d489f8ff8930577db3cb15f713644ffede75
Niels Boecker <nielsboecker@gmail.com>
2019-03-19 19:13:43 +0000

    #2: Improve WikiDocument print() representation

--- src/wiki_document.py
+++ src/wiki_document.py
@@ -1,5 +1,7 @@
 import json
 
+from termcolor import colored
+
 
 def extract_lines(lines_json_string: str):
     raw_lines = lines_json_string.split('\n')
@@ -17,22 +19,26 @@ def extract_lines(lines_json_string: str):
 
 
 class WikiDocument:
-  def __init__(self, doc_json_string):
-    # Use raw string to account for \t and \n characters
-    raw_doc_json_string = r"{}".format(doc_json_string)
-    data = json.loads(raw_doc_json_string, encoding='asciixx')
-    # id, foo = extract_data(doc_json_string)
-    self.id = data['id']
-    self.text = data['text']
-    self.lines = extract_lines(data['lines'])
+    def __init__(self, doc_json_string):
+        # Use raw string to account for \t and \n characters
+        raw_doc_json_string = r"{}".format(doc_json_string)
+        data = json.loads(raw_doc_json_string, encoding='asciixx')
+        # id, foo = extract_data(doc_json_string)
+        self.id = data['id']
+        self.text = data['text']
+        self.lines = extract_lines(data['lines'])
+
+    def __str__(self):
+        return 'ID:\t\t{}\nTEXT:\t{}\n'.format(colored(self.id, attrs=['bold']), colored(self.text, attrs=['underline']))
 
 
 class WikiLine:
-  def __init__(self, index, text, anchors):
-      self.index = index
-      self.text = text
-      self.anchors = anchors
+    def __init__(self, index, text, anchors):
+        self.index = index
+        self.text = text
+        self.anchors = anchors
 
 
-# TODO: make sure comes as raw string
-p1 = WikiDocument(r'{"id": "Hello", "text": "Hello is a salutation or greeting in the English language . It is first attested in writing from 1826 . ", "lines": "0\tHello is a salutation or greeting in the English language .\tsalutation\tsalutation\tgreeting\tgreeting habits\tEnglish language\tEnglish language\n1\tIt is first attested in writing from 1826 .\n2\t"}')
+test_doc = WikiDocument(
+    r'{"id": "Hello", "text": "Hello is a salutation or greeting in the English language . It is first attested in writing from 1826 . ", "lines": "0\tHello is a salutation or greeting in the English language .\tsalutation\tsalutation\tgreeting\tgreeting habits\tEnglish language\tEnglish language\n1\tIt is first attested in writing from 1826 .\n2\t"}')
+print(test_doc)
