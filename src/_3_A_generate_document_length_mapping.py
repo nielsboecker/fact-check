e9e3ecbc1bad1ce64f149ed3382e93102bf2329c
Niels Boecker <nielsboecker@gmail.com>
2019-03-24 18:26:22 +0000

    #3: Generate document->length mapping

---
+++ src/_3_A_generate_document_length_mapping.py
@@ -0,0 +1,48 @@
+import argparse
+import time
+
+from dataaccess.constants import GENERATED_DOCUMENT_LENGTH_MAPPING, get_wiki_batch_path
+from dataaccess.json_io import read_jsonl_and_map_to_df, write_dict_to_jsonl
+from documentretrieval.document_processing import filter_documents
+from documentretrieval.term_processing import process_normalise_tokenise_filter
+from util.theads_processes import get_process_pool
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--debug", help="only use subset of data", action="store_true")
+args = parser.parse_args()
+
+
+def generate_document_length_mapping_for_batch(batch_id: int) -> dict:
+    batch_file_path = get_wiki_batch_path(batch_id)
+    all_articles = read_jsonl_and_map_to_df(batch_file_path, ['id', 'text'])
+    filtered_articles = filter_documents(all_articles)
+    # filtered_articles.set_index('id', drop=False)
+    if (args.debug):
+        filtered_articles = filtered_articles.head(n=3)
+
+    partial_document_length_mappings = {}
+    for raw_doc_row in filtered_articles.iterrows():
+        page_id = raw_doc_row[1]['id']
+        filtered_tokens = process_normalise_tokenise_filter(raw_doc_row[1]['text'])
+        partial_document_length_mappings[page_id] = len(filtered_tokens)
+
+    return partial_document_length_mappings
+
+
+def generate_document_length_mapping_all() -> dict:
+    start_index_inclusive = 108 if args.debug else 1
+    stop_index_exclusive = 110
+
+    process_pool = get_process_pool()
+    batch_partial_counts = process_pool.map(generate_document_length_mapping_for_batch, range(start_index_inclusive, stop_index_exclusive))
+    accumulated_document_length_mapping = {}
+    for batch_result in batch_partial_counts:
+        accumulated_document_length_mapping.update(batch_result)
+    return accumulated_document_length_mapping
+
+
+if __name__ == '__main__':
+    start_time = time.time()
+    document_norm_mapping = generate_document_length_mapping_all()
+    print('Finished generating document -> length mapping after {:.2f} seconds'.format(time.time() - start_time))
+    write_dict_to_jsonl(GENERATED_DOCUMENT_LENGTH_MAPPING, document_norm_mapping)
--- src/documentretrieval/access_inverted_index.py
+++ src/documentretrieval/access_inverted_index.py
@@ -12,3 +12,17 @@ def get_index_entry_for_term(term: str) -> dict:
     shard_id = get_inverted_index_shard_id(term)
     shard = read_inverted_index_shard(shard_id)
     return shard[term]
+
+
+def get_candidate_documents_for_claim(claim_terms) -> dict:
+    doc_candidates = {}
+    # In the index, for each term, the occurrences per document are stored
+    # For all terms, group by document to compute TF values
+    for term in claim_terms:
+        index_entry = get_index_entry_for_term(term)
+        docs = index_entry['docs']
+        for doc in docs:
+            page_id = doc[0]
+            tfidf_for_term = doc[1]
+            doc_candidates.setdefault(page_id, {})[term] = tfidf_for_term
+    return doc_candidates
