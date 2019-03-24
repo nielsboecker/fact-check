b4871ed46076dbc05fe905402daf46cd66893c72
Niels Boecker <nielsboecker@gmail.com>
2019-03-24 17:23:40 +0000

    Reduce multiprocessing by one core

--- src/util/theads_processes.py
+++ src/util/theads_processes.py
@@ -5,10 +5,11 @@ from multiprocessing.pool import ThreadPool
 def get_process_pool():
     # Process in multiple blocking processes
     print(('Detected {} CPUs'.format(cpu_count())))
-    pool = Pool(processes=cpu_count())
+    pool = Pool(processes=cpu_count()-1)
     return pool
 
 
 def get_thread_pool():
-    thread_pool = ThreadPool(processes=cpu_count())
+    thread_pool = ThreadPool(processes=cpu_count()-1)
     return thread_pool
+
