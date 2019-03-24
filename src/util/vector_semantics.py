621f3d775389be1ffad09501e668935da71c91a6
Niels Boecker <nielsboecker@gmail.com>
2019-03-24 17:30:13 +0000

    #2: Read IDF values from word->IDF mapping rather than inverted index

--- src/util/vector_semantics.py
+++ src/util/vector_semantics.py
@@ -1,17 +1,19 @@
 import math
 from collections import Counter
 
-from documentretrieval.access_inverted_index import get_index_entry_for_term
+from dataaccess.constants import GENERATED_IDF_PATH
+from dataaccess.json_io import read_jsonl_and_map_to_df
+
+words_with_idf = read_jsonl_and_map_to_df(GENERATED_IDF_PATH, ['word', 'idf']).set_index('word', drop=False)
 
 
 def get_tfidf_vector_norm(text: list, debug=False, variant='relative'):
-    # Note: this takes the TF directly from the text argument
+    # Note: this takes the TF directly from the text argument and IDF from doc -> IDF mapping
     word_count = Counter(text)
     accumulated_tfidf_values_for_words = []
     for word, count in word_count.items():
         tf = count if variant == 'raw_count' else float(count) / len(text)
-        index_entry = get_index_entry_for_term(word)
-        idf = index_entry['idf']
+        idf = words_with_idf.loc[word]['idf']
         tfidf_value = tf * idf
         accumulated_tfidf_values_for_words.append(tfidf_value)
     norm = math.sqrt(sum([i ** 2 for i in accumulated_tfidf_values_for_words]))
