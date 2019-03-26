ec84c5f53fad5a0375cea23221043d0e2c10399e
Niels Boecker <nielsboecker@gmail.com>
2019-03-26 01:08:27 +0000

    Add helper scripts to run retrieval

---
+++ retrieval_scripts/retrieve_docs_for_first_train_claims.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+python3 src/_2_F_retrieve_docs_with_tfidf.py --limit
+echo ">>>>> TFIDF retrieval finished"
+
+python3 src/_3_F_retrieve_docs_with_unigram_query_likelihood.py --limit
+echo ">>>>> Bare query likelihood retrieval finished"
+
+python3 src/_3_F_retrieve_docs_with_unigram_query_likelihood.py --limit --smoothing "laplace"
+echo ">>>>> laplace retrieval finished"
+
+python3 src/_3_F_retrieve_docs_with_unigram_query_likelihood.py --limit --smoothing "laplace_lindstone"
+echo ">>>>> laplace_lindstone retrieval finished"
+
+python3 src/_3_F_retrieve_docs_with_unigram_query_likelihood.py --limit --smoothing "jelinek_mercer"
+echo ">>>>> jelinek_mercer retrieval finished"
+
+# python3 src/_3_F_retrieve_docs_with_unigram_query_likelihood.py --limit --smoothing "dirichlet"
