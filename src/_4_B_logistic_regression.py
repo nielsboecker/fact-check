283b4006ba531d7dcb197a2a12d1da7cbfea9b85
Niels Boecker <nielsboecker@gmail.com>
2019-03-31 02:09:11 +0100

    #4: Add intercept

--- src/_4_B_logistic_regression.py
+++ src/_4_B_logistic_regression.py
@@ -12,13 +12,14 @@ class LogisticRegressionModel:
         self.weights = weights
 
 
-    def predict(self, in_values: np.ndarray, threshold: float = 0.5) -> np.array:
-        z = np.dot(in_values, self.weights)
-        hypothesis = sigmoid(z)
+    def predict(self, in_values: ndarray, threshold: float = 0.5) -> ndarray:
+        in_values = prepend_intercept(in_values)
+        lin_regression = np.dot(in_values, self.weights)
+        hypothesis = sigmoid(lin_regression)
         return hypothesis >= threshold
 
 
-def sigmoid(values: np.ndarray) -> np.ndarray:
+def sigmoid(values: ndarray) -> ndarray:
     return 1 / (1 + np.exp(-values))
 
 
@@ -26,11 +27,17 @@ def get_loss(hypothesis: ndarray, actual: ndarray) -> float: # TODO
     return (-actual * np.log(hypothesis) - (1 - actual) * np.log(1 - hypothesis)).mean()
 
 
+def prepend_intercept(values: ndarray) -> ndarray:
+    intercept = np.ones((values.shape[0], 1))
+    return np.concatenate((intercept, values), axis=1)
+
+
 def fit_and_get_model(in_values: ndarray, out_expected: ndarray, debug: bool = False) -> tuple:
     global NUM_EPOCHS, LEARNING_RATE
-    # TODO: intercept!
     start_time = time.time()
     print('Start logistc regression fitting')
+
+    in_values = prepend_intercept(in_values)
 
     dimension = in_values.shape[1]
     num_of_datapoints = len(out_expected) * dimension # out_expected.size
@@ -46,16 +53,15 @@ def fit_and_get_model(in_values: ndarray, out_expected: ndarray, debug: bool = F
 
     # improve weights to fit expected output better
     for i in range(NUM_EPOCHS):
-        z = np.dot(in_values, weights)
-        hypothesis = sigmoid(z)
+        lin_regression = np.dot(in_values, weights)
+        hypothesis = sigmoid(lin_regression)
 
-        # TODO T vs transpose
         difference = hypothesis - out_expected
         gradient = np.dot(in_values.transpose(), difference) / num_of_datapoints
         weights -= LEARNING_RATE * gradient
 
         # TODO
-        if i % 100 == 0:
+        if i % 500 == 0:
             loss_values.append(get_loss(hypothesis, out_expected))
 
     print('Finished fitting after {:.2f} seconds\nFinal weights: {}'.format(time.time() - start_time, weights))
