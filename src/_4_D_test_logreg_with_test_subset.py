d4b3945e1736676069ac417168d17bb007cdaef0
Niels Boecker <nielsboecker@gmail.com>
2019-03-31 13:40:13 +0100

    #4: Add baserate model to compare

--- src/_4_D_test_logreg_with_test_subset.py
+++ src/_4_D_test_logreg_with_test_subset.py
@@ -1,15 +1,9 @@
-import numpy as np
-
 from _4_B_logistic_regression import LogisticRegressionModel
 from dataaccess.constants import GENERATED_PREPROCESSED_DEV_DATA, \
     GENERATED_LOGISTIC_REGRESSION_MODEL
 from dataaccess.files_io import read_pickle
+from util.evaluation import get_accuracy, get_baserate_prediction
 from util.logreg_preprocessing import extract_input_and_expected
-
-
-def get_accuracy(predicted: np.ndarray, actual: np.ndarray) -> float:
-    correspondence = predicted == actual
-    return correspondence.mean()
 
 
 if __name__ == '__main__':
@@ -17,6 +11,11 @@ if __name__ == '__main__':
     dev_data = read_pickle(GENERATED_PREPROCESSED_DEV_DATA)
     dev_input, dev_expected = extract_input_and_expected(dev_data)
 
-    dev_predicted = model.predict(dev_input)
-    accuracy = get_accuracy(dev_predicted, dev_expected)
-    print(accuracy)
+    model_prediction = model.predict(dev_input)
+    model_accuracy = get_accuracy(model_prediction, dev_expected)
+
+    baserate_prediction = get_baserate_prediction(model_prediction)
+    baserate_accuracy = get_accuracy(baserate_prediction, dev_expected)
+
+    print('Trained model accuracy: {:.5f}'.format(model_accuracy))
+    print('Base-rate accuracy: {:.5f}'.format(baserate_accuracy))
