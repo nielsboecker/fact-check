46b5b7c38da4c9662cb9287b131b3f30abbcb820
Niels Boecker <nielsboecker@gmail.com>
2019-04-01 18:18:56 +0100

    #1: Improve plot

--- src/_1_B_prove_zipf_with_linear_regression.py
+++ src/_1_B_prove_zipf_with_linear_regression.py
@@ -1,4 +1,5 @@
 import math
+from decimal import Decimal
 
 import matplotlib.pyplot as plt
 from scipy import stats
@@ -7,8 +8,7 @@ from dataaccess.files_constants import GENERATED_COUNTS_PATH
 from dataaccess.files_io import read_jsonl_and_map_to_df
 from util.plots import show_plot_and_save_figure, prepare_seaborn_plots
 
-
-def verify_plot_zipfs_law():
+if __name__ == '__main__':
     counts = read_jsonl_and_map_to_df(GENERATED_COUNTS_PATH)
 
     x_ranks = range(1, len(counts[1]) + 1)
@@ -18,19 +18,16 @@ def verify_plot_zipfs_law():
 
     slope, intercept, r_value, p_value, std_err = stats.linregress(x_ranks_log, y_counts_log)
     r_squared = r_value ** 2
-    print('slope: {}; intercept: {}; r-squared: {}'.format(slope, intercept, r_squared))
+    print('slope: {}; intercept: {}; r-squared: {}, p: {}'.format(slope, intercept, r_squared, p_value))
 
     prepare_seaborn_plots()
-    plt.plot(x_ranks_log, y_counts_log, 'o')  # label='word frequencies')
-    plt.plot(x_ranks_log, [intercept + slope * rank for rank in x_ranks_log], 'red')  # , label='fitted line')
+    plt.plot(x_ranks_log, y_counts_log, 'o')
+    plt.plot(x_ranks_log, [intercept + slope * rank for rank in x_ranks_log], 'red')
 
-    plt.xlabel('$log(rank)$')
-    plt.ylabel('$log(frequency)$')
-    plt.figtext(0.3, 0.45, '$R^2 = {:.5f}$'.format(r_squared))
-    # plt.legend()
+    plt.xlabel('log(rank)')
+    plt.ylabel('log(frequency)')
+    plt.figtext(0.2, 0.45, 'R$^2$ = {:.5f}'.format(r_squared))
+    plt.figtext(0.2, 0.4, 'p < 4E-324')
+    plt.figtext(0.2, 0.35, 'standard error = {:.3E}'.format(Decimal(std_err)))
 
     show_plot_and_save_figure('1_distribution_zipf.png')
-
-
-if __name__ == '__main__':
-    verify_plot_zipfs_law()
